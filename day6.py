867da11bee84f8404031393f2bbd43944a725e72
Vladyslav Knysh <knyshv2412@gmail.com>
2023-12-08 19:05:32 +0200

    day7.2

--- day6.py
+++ day6.py
@@ -5,37 +5,65 @@ import math
 from typing import List
 import requests
 import re
+from collections import Counter
+
+
 
 @dataclass
-class Race:
-    time: int
-    distance: int
+class Hand:
+    cards: str
+    bid: int
+    
+    reg = re.compile('(?P<hand>[\d|A|K|Q|J|T]{5})\s(?P<bid>\d+)')
+    @classmethod
+    def from_str(cls, str):
+        m = cls.reg.match(str)
+        return cls(m['hand'], int(m['bid']))
     
     @property
-    def min_int_time(self):
-        t = self.time
-        d = self.distance
-        return math.ceil((t - math.sqrt(t**2 - 4*d)) / 2)
-    
-    @property
-    def max_int_time(self):
-        t = self.time
-        d = self.distance
-        return math.floor((t + math.sqrt(t**2 - 4*d)) / 2)
-    
-    @property
-    def possible_solutions(self):
-        res = self.max_int_time - self.min_int_time+1
-        return res
+    def htype(self):
+        cards = self.cards
+        if cards == "JJJJJ":
+            return 7
+        if "J" in cards:
+            counter = Counter(self.cards.replace('J', ''))
+            most_common = counter.most_common(1)
+            cards = cards.replace("J", most_common[0][0])
         
-        # a = self.min_int_time
-        # b = self.max_int_time
-        # res = (a+b)*(b-a+1)/2 
+        
+        counts = list(Counter(cards).values())
+                 
+        
+        if 5 in counts:
+            rank = 7
+        elif 4 in counts:
+            rank = 6
+        elif 3 in counts and 2 in counts:
+            rank = 5
+        elif 3 in counts and 2 not in counts:
+            rank = 4
+        elif counts.count(2) == 2:
+            rank = 3
+        elif counts.count(2) == 1:
+            rank = 2
+        else:
+            rank = 1
+        return rank
     
+    @property
+    def hand_values(self):
+        orig = 'AKQT98765432J'
+        values = 'CBA9876543210'
+        m={k:v for k,v in zip(orig, values)}
+        return ''.join([m[s] for s in self.cards])
+           
+    @property                   
+    def sort_key(self) -> str:
+        return f"{self.htype} {self.hand_values}"
 
 if __name__ == "__main__":
 
-    input = requests.get('https://adventofcode.com/2023/day/6/input', headers={
+    input = requests.get('https://adventofcode.com/2023/day/7/input', headers={
         "Host": "adventofcode.com",
         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
@@ -51,24 +79,20 @@ if __name__ == "__main__":
         "TE": "trailers"
     })
 
-    data = input.content.decode('utf-8')
-    input_regex = re.compile('Time:\s*?(?P<time>[\d\s]*)\nDistance:\s*?(?P<Distance>[\d\s]*)')
-    input_match = input_regex.match(data)
+    hands = []
+    data = input.content.decode('utf-8').split('\n')
+    for i, row in enumerate([d for d in data if d]):
+        h = Hand.from_str(row)
+        bisect.insort(hands, h, key=lambda h: h.sort_key)
     
-    # races = [Race(i[0], i[1]) for i in zip(
-    #     [int(t) for t in input_match['time'].replace(' ', '') if t],
-    #     [int(d) for d in input_match['Distance'].replace(' ', '') if d],        
-    # )]
-    # races[0].max_int_time
+    res = 0
+    for i, hand in enumerate(hands):
+        res += (i+1)*hand.bid
+    print(res)    
     
-    races = [Race(
-        int(input_match['time'].replace(' ', '')),
-        int(input_match['Distance'].replace(' ', ''))
-    )]
-    r = 1
-    for i in races:
-        r *= i.possible_solutions
-    print(r)
+
+
+
     
     
     
